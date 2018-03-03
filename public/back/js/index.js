7bfb3d24288f88a31c51a22746f5bf8e622a0957
janeeyrehome <996511228@qq.com>
2018-03-03 23:06:12 +0800

    index的静态页面

---
+++ public/back/js/index.js
@@ -0,0 +1,71 @@
+/**
+ * Created by STAR on 2018-03-03.
+ */
+$(function(){
+    var myChart = echarts.init(document.querySelector('.charts_1'));
+    var myChart1 = echarts.init(document.querySelector('.charts_2'));
+
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: '2017年注册人数'
+        },
+        tooltip: {},
+        legend: {
+            data:['人数']
+        },
+        xAxis: {
+            data: ["1月","2月","3月","4月","5月","6月"]
+        },
+        yAxis: {},
+        series: [{
+            name: '人数',
+            type: 'bar',
+            data: [1005, 2000, 3006, 1008, 1340, 2890]
+        }]
+    };
+
+   var option1 = {
+        title : {
+            text: '热门品牌销售',
+            subtext: '2017年6月',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['耐克','阿迪','百伦','百伦','安踏']
+        },
+        series : [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius : '55%',
+                center: ['50%', '60%'],
+                data:[
+                    {value:335, name:'耐克'},
+                    {value:310, name:'阿迪'},
+                    {value:234, name:'百伦'},
+                    {value:135, name:'百伦'},
+                    {value:1548, name:'安踏'}
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+    myChart1.setOption(option1);
+})
